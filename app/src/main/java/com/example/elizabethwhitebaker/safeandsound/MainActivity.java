72fcdd9b91b253f346ce72ef1868317665ffb2fe
Jeffrey <duahjeffrey08@gmail.com>
2020-09-12 22:08:11 -0400

    Revert "Revert "Upgrade minimum sdk""
    This reverts commit 70791fdc104766fa55674e95d4d3c6a5664bc488.

--- app/src/main/java/com/example/elizabethwhitebaker/safeandsound/MainActivity.java
+++ app/src/main/java/com/example/elizabethwhitebaker/safeandsound/MainActivity.java
@@ -3,8 +3,8 @@ package com.example.elizabethwhitebaker.safeandsound;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -139,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new android.support.v7.app.AlertDialog.Builder(MainActivity.this)
+        new androidx.appcompat.app.AlertDialog.Builder(MainActivity.this)
                 .setMessage(message)
                 .setPositiveButton("OK", okListener)
                 .setNegativeButton("Cancel", null)
