0051e1195026ae9c94bf95b760598af13ef0d408
Jeffrey <duahjeffrey08@gmail.com>
2020-10-11 19:43:51 -0400

    add back button for profile

--- app/src/main/java/com/example/elizabethwhitebaker/safeandsound/ProfileActivity.java
+++ app/src/main/java/com/example/elizabethwhitebaker/safeandsound/ProfileActivity.java
@@ -2,11 +2,15 @@ package com.example.elizabethwhitebaker.safeandsound;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
     private int initID;
+    private String name;
     private DBHandler handler = new DBHandler(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +18,26 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         initID = getIntent().getIntExtra("initID", 0);
+        name = getIntent().getStringExtra("name");
+
         final String name = getIntent().getStringExtra("name");
         final String user = getIntent().getStringExtra("user");
         final String pass = getIntent().getStringExtra("pass");
 
         Initiator userProfile = handler.findHandler(user, pass);
 
+        Button goBack = findViewById(R.id.go_back_button);
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, HomeScreenActivity.class);
+                intent.putExtra("initID", initID);
+                intent.putExtra("name", name);
+                startActivity(intent);
+
+            }
+        });
 
 
     }
